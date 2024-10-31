1f1ac016400330c9254d37a1b0ff2f9a0c3382ef
jgarnet <jgarnett18@gmail.com>
2024-10-30 21:00:06 -0400

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import BaseExpressionEvaluator from "./base-expression-evaluator";
 import BaseExpressionParser from "./base-expression-parser";
 import BaseFunctionEvaluator from "./base-function-evaluator";
 import BasePathEvaluator from "./base-path-evaluator";
+import ComparisonOperator from "./types/comparison-operator";
 import ConditionEvaluator from "./types/condition-evaluator";
 import createContext from "./create-context";
 import ExpressionContext from "./types/expression-context";
@@ -11,15 +12,16 @@ import ExpressionParser from "./types/expression-parser";
 import ExpressionFunction from "./types/expression-function";
 import functions from "./functions/_functions";
 import FunctionEvaluator from "./types/function-evaluator";
-import ComparisonOperator from "./types/comparison-operator";
 import operators from "./operators/_operators";
 import PathEvaluator from "./types/path-evaluator";
+import * as utils from './_utils';
 
 export {
     BaseConditionEvaluator,
     BaseExpressionParser,
-    BasePathEvaluator,
     BaseFunctionEvaluator,
+    BasePathEvaluator,
+    ComparisonOperator,
     ConditionEvaluator,
     createContext,
     ExpressionContext,
@@ -28,9 +30,9 @@ export {
     ExpressionFunction,
     functions,
     FunctionEvaluator,
-    ComparisonOperator,
     operators,
-    PathEvaluator
+    PathEvaluator,
+    utils
 };
 
 export default BaseExpressionEvaluator;
