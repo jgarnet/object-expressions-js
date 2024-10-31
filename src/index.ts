1ac3d288cb2f295f98154aa52fb0a2345f50fdb8
jgarnet <jgarnett18@gmail.com>
2024-10-30 20:58:07 -0400

    added function evaluator to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import BaseConditionEvaluator from "./base-condition-evaluator";
 import BaseExpressionEvaluator from "./base-expression-evaluator";
 import BaseExpressionParser from "./base-expression-parser";
+import BaseFunctionEvaluator from "./base-function-evaluator";
 import BasePathEvaluator from "./base-path-evaluator";
 import ConditionEvaluator from "./types/condition-evaluator";
 import createContext from "./create-context";
@@ -9,6 +10,7 @@ import ExpressionEvaluator from "./types/expression-evaluator";
 import ExpressionParser from "./types/expression-parser";
 import ExpressionFunction from "./types/expression-function";
 import functions from "./functions/_functions";
+import FunctionEvaluator from "./types/function-evaluator";
 import ComparisonOperator from "./types/comparison-operator";
 import operators from "./operators/_operators";
 import PathEvaluator from "./types/path-evaluator";
@@ -17,6 +19,7 @@ export {
     BaseConditionEvaluator,
     BaseExpressionParser,
     BasePathEvaluator,
+    BaseFunctionEvaluator,
     ConditionEvaluator,
     createContext,
     ExpressionContext,
@@ -24,6 +27,7 @@ export {
     ExpressionParser,
     ExpressionFunction,
     functions,
+    FunctionEvaluator,
     ComparisonOperator,
     operators,
     PathEvaluator
