c8b06cf556427a2dd6346a2c377a2ec20fa0f64e
jgarnet <jgarnett18@gmail.com>
2024-12-24 10:10:58 -0500

    chore: ensure release script exits if git push fails

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,6 +5,6 @@ yarn build || exit 1
 # calculate current semver and create release tag
 ./node_modules/.bin/standard-version || exit 1
 # push to remote repo
-git push
+git push || exit 1
 # publish package to npm
 npm publish
